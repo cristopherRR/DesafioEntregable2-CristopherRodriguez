730d92ff65d0652a21b7c3e4a39ca2c9155105d7
cristopherRR <cristohper.rr@gmail.com>
2023-12-21 00:04:22 -0400

    segundo desafio

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
-class ProductManager {
-    constructor() {
-        this.products = [];
-        this.newId = 1;
-    }
+const ProductManager = require('./ProductManager');
 
-    addProduct(title, description, price, tumbnail, code, stock) {
-
-        const productoExistencia = this.products.find((producto) => producto.code === code)
-        if (productoExistencia) {
-            console.log(`El producto ${title} ya existe, el codigo ${code} es el mismo al del ${productoExistencia.title}.`);
-            return;
-        }
-        
-        if (!title || !description || !price || !tumbnail || !code || !stock) {
-            console.log('los campos son obligatorios en el producto')
-        }else {
-            const producto = {
-                id: this.newId++,
-                title,
-                description,
-                price,
-                tumbnail,
-                code,
-                stock
-            };
-            this.products.push(producto); 
-            
-            console.log(`El producto ${producto.title} se agrego`);
-        }
-    }
-
-    getProduct() {
-        return this.products;
-    }
-    getProductById(id) {
-        const productoId = this.products.find((producto) => producto.id === id); 
-            if (!productoId) {
-                console.log('Not Found');
-            }else {
-                console.log('Se encontro el producto buscado')
-                return productoId;
-            }
-    }
-}
-
-const producto = new ProductManager()
+const producto = new ProductManager('./products.json');
 producto.addProduct('Puente cardan', '40mm isuzu npr', 40, 'img', 'code-2302', 5);
 producto.addProduct('Estopera', 'de cigueñal interna isuzu npr', 10, 'img', 'code-2304', 10);
-producto.addProduct('anillo', '45mm isuzu npr', 20, 'img', 'code-2302', 12);
+producto.addProduct('Anillo', '45mm isuzu npr', 20, 'img', 'code-2302', 12);
+producto.addProduct('Turbo', 'izusu npr 4HG1', 150, 'img', 'code-2303', 5);
 
-console.log(producto.getProduct());
+console.log(producto.getProducts());
 
 producto.getProductById(1);
+
+producto.updateProduct(2, { stock: 20 });
+
+producto.deleteProduct(3);
+
+
+console.log(producto.getProducts());
